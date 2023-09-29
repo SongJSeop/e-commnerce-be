6476a1185d99cf473122adf3fad82820a75a2613
송준섭 <wnstjq7979@gmail.com>
2023-09-29 19:33:46 +0900

    feat: 메모리를 이용한 간단한 item 조회 api 구현
    ItemService에 임시 items 배열 변수 생성
    getAllItems() 메소드로 items 변수 리턴
    ItemController에서 @Get() 으로 매핑 후 값 리턴

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 import { ItemService } from "./item.service";
 
 @Controller('item')
 export class ItemController {
+
   constructor(private itemService: ItemService) {}
+
+  @Get()
+  getAllItems() {
+    return this.itemService.getAllItems();
+  }
 }
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ItemService {}
+export class ItemService {
+
+  private items = [{
+    id: 1,
+    name: 'Burger',
+    price: 5.99,
+  }, {
+    id: 2,
+    name: 'Omelette',
+    price: 6.99,
+  }];
+
+  getAllItems() {
+    return this.items;
+  }
+}
