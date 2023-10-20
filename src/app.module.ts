f56e81133350e26ac8077b8b9fcba17b501ba9d2
송준섭 <wnstjq7979@gmail.com>
2023-10-21 04:50:48 +0900

    feat: 인증을 위한 auth 모듈, 컨트롤러, 서비스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { BoardModule } from './board/board.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
 import { DataSource } from 'typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule],
+    imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule, AuthModule],
 })
 export class AppModule {
     constructor(private dataSource: DataSource) {}
