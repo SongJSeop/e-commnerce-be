bc23efbd41ec8911748ff23cd9e92ec019fe68b3
송준섭 <wnstjq7979@gmail.com>
2023-10-24 15:53:13 +0900

    feat: 모든 회원 조회, 회원 삭제 api 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Body, Get, Post, ValidationPipe } from '@nestjs/common';
+import {
+    Controller,
+    Body,
+    Get,
+    Post,
+    ValidationPipe,
+    Delete,
+    Param,
+    ParseIntPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import { DeleteResult } from 'typeorm';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Get()
+    getUsers(): Promise<User[]> {
+        return this.authService.getUsers();
+    }
 
     @Post('/signup')
     signUp(
         @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
     ): Promise<User> {
         return this.authService.signUp(authCredentialsDto);
+    }
+
+    @Delete('/:id')
+    deleteUserById(
+        @Param('id', ParseIntPipe) id: number,
+    ): Promise<DeleteResult> {
+        return this.authService.deleteUserById(id);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { DeleteResult } from 'typeorm';
 
 @Injectable()
 export class AuthService {
     constructor(private userRepository: UserRepository) {}
 
+    async getUsers(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
         return this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async deleteUserById(id: number): Promise<DeleteResult> {
+        return await this.userRepository.delete(id);
     }
 }
